361f8436bfd7a5669f90d7c4f086678976a57a5f
Muhamad Danial Khalid <90670465+DanKhld@users.noreply.github.com>
2023-07-03 14:09:21 +0800

    Add files via upload

---
+++ Hotel Data Revenue.sql
@@ -0,0 +1,13 @@
+with hotels as (
+select * from dbo.['2018$']
+union all
+select * from dbo.['2019$']
+union all
+select * from dbo.['2020$'])
+
+
+select * from hotels
+left join market_segment$
+on hotels.market_segment = market_segment$.market_segment
+left join meal_cost$
+on meal_cost$.meal = hotels.meal
